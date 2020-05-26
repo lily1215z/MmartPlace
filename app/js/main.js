c197879d317a29054826e4286405c3fc2e205e69
lily1215z <odessa1215@i.ua>
2020-05-26 22:08:32 +0300

    added section slider and icon in header

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
 $(function(){
 
+    $(".slider__btn-rate").rateYo({
+        rating: 4.6,
+        starWidth:"15px"
+      });
 
+    $('.slider__inner').slick({
+        dots:false,
+        arrows:true,
+    });
 });
